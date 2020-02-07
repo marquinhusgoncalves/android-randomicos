96fb4f81dde78782430ab7dfc9aa2e971ed0e9d1
Marquinhus Goncalves <eu@marquinhusgoncalves.com>
2020-02-06 22:45:11 -0300

    chore: click events

--- app/src/main/java/com/example/randomicos/MainActivity.kt
+++ app/src/main/java/com/example/randomicos/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.randomicos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonRandom.setOnClickListener(this)
+        textNumber.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if(v?.id == R.id.buttonRandom || v?.id == R.id.textNumber) {
+            
+        }
     }
 }
